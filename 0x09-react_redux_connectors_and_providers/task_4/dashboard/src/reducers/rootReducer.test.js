947e8ea50ecc715c2e11ac877117346771f2cac1
raniachikhi <raniach0653@gmail.com>
2024-05-12 06:16:43 -0700

    add

---
+++ 0x09-react_redux_connectors_and_providers/task_4/dashboard/src/reducers/rootReducer.test.js
@@ -0,0 +1,14 @@
+import rootReducer from './rootReducer';
+
+describe('rootReducer', () => {
+  it('should return the initial state', () => {
+    const initialState = {
+      courses: new Map(),
+      notifications: new Map(),
+      ui: new Map()
+    };
+
+    expect(rootReducer(undefined, {})).toEqual(initialState);
+  });
+});
+
