bd5552b1a30ea4a63b3e03082742177fdcd71866
raniachikhi <raniach0653@gmail.com>
2024-05-12 05:33:17 -0700

    add

--- 0x09-react_redux_connectors_and_providers/task_3/dashboard/src/index.js
+++ 0x09-react_redux_connectors_and_providers/task_3/dashboard/src/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App/App";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
 import uiReducer from "./reducers/uiReducer";
 import thunk from 'redux-thunk'
 import { Map } from 'immutable'
-import { composeWihDevTools } from "redux-devtools-extension"
+import { composeWihDevTools } from "redux-devtools-extension";
+import rootReducer, { initialState } from "./reducers/rootReducer";
 
 const reduxExtension = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || composeWihDevTools;
-const store = createStore(uiReducer, Map(initialState), reduxExtension(applyMiddleware(thunk)));
+const store = createStore(combineReducers(rootReducer), initialState, reduxExtension(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <React.StrictMode>
